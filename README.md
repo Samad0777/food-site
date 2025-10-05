cba7e65abed375c0f065914e7016fd9c8ce1f495
samad <sheikfardeen6@gmail.com>
2025-10-05 22:37:17 +0530

    feat: Book a table page is added using Material UI library

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Focus on writing clean, reusable components
 ✅ Home page Completed
 ✅ About page Completed
 ✅ Menu page is Completed
-🚧 Book Table page is ongoing
+✅ Book Table page is Adedd
 🚧 Responsive layout (applied on completed sections, rest ongoing)
 🚧 Contact Page , Blog page (next on the list)
 
--- src/pages/BookTable.jsx
+++ src/pages/BookTable.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Map from "../assets/images/Map.png";
+import DatePicker from "../components/DatePicker";
+import TimePicker from "../components/TimePicker";
 
 const BookTable = () => {
   return (
     <>
-      <section className="h-auto relative border border-b-green-700">
-        <div className="border border-red-700 text-center py-8">
+      <section className="h-auto relative">
+        <div className=" text-center py-8">
           <h1 className="font-playfair text-secondary text-4xl md:text-6xl">
             Book A Table
           </h1>
@@ -16,22 +18,50 @@ const BookTable = () => {
         </div>
 
         {/* booking section started  */}
-        {/* <div className="absolute border border-green-700 bg-white">
-        <form action="#">
-            <input type="date" name="" id="" />
-            <input type="time" name="time" id="" />
-            <input type="text" placeholder="Enter your name" />
-            <input type="text" placeholder="x-xxx-xxx-xxxx"/>
-            <details>
-                <summary>
-                    1 person
-                </summary>
-                <summary>
-                    2 person
-                </summary>
-            </details>
-        </form>
-        </div> */}
+        <div className="flex justify-center items-center">
+          <form className="flex flex-col space-x-4 space-y-4  md:px-8 md:py-10">
+            <div className="flex flex-col md:flex-row justify-between md:items-center space-x-8 space-y-4">
+              <label htmlFor="DatePicker">Date</label>
+              <DatePicker/>
+              <label htmlFor="TimePicker">Time</label>
+              <TimePicker />
+            </div>
+
+            <div className="flex flex-col md:flex-row justify-between md:items-center space-x-8 space-y-4">
+              <label htmlFor="Name">Name</label>
+              <input
+                type="text"
+                name="Name"
+                placeholder="Enter your name"
+                className="border border-gray-400 px-8 py-4 rounded-sm md:w-[60%] w-full"
+              />
+
+              <label htmlFor="Phone">Phone</label>
+              <input
+                type="text"
+                name="Phone"
+                placeholder="x-xxx-xxx-xxxx"
+                className="border px-8 py-4 border-gray-400 rounded-sm md:w-[60%] w-full"
+              />
+            </div>
+
+            <label className="py-4" htmlFor="member">Total members</label>
+            <input
+              type="number"
+              name="member"
+              placeholder="Enter members"
+              className="border px-8 py-4 w-full border-gray-400 rounded-sm"
+            />
+
+            <button
+              type="submit"
+              className="my-4 cursor-pointer hover:bg-[#972730] transition-all ease-in-out duration-300 active:scale-90 w-full bg-red-primary text-white py-4 px-4 rounded-3xl"
+            >
+              Book A Table
+            </button>
+          </form>
+        </div>
+
         {/* booking section ended  */}
 
         <div
