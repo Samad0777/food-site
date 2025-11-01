e1bc56a4149aa0ac5869c3d715cb063d713da6a5
samad <sheikfardeen6@gmail.com>
2025-11-01 22:33:30 +0530

    feat: added new page for 404 page not dound and also change it the routing method

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import Navbar from './components/Navbar'
-import { Route,Routes } from 'react-router-dom'
+import { createBrowserRouter,Outlet,RouterProvider } from "react-router";
 import Home from './pages/Home'
 import About from './pages/About'
 import Menu from './pages/Menu'
@@ -8,26 +7,49 @@ import Pages from './pages/Pages'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
 import BookTable from './pages/BookTable'
+import Erorr404 from './pages/Erorr404';
 
+const router = createBrowserRouter([
+  { 
+    path: "/",
+    element: <div><Navbar/> <Outlet/><Footer/></div>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/menu",
+        element:<Menu/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      },
+      {
+        path:"/pages",
+        element:<Pages/>
+      },
+      {
+        path:"/booktable",
+        element:<BookTable/>
+      },
+      {
+        path:"*",
+        element:<Erorr404/>
+      },
+    ]
+   },
+]);
 
 const App = () => {
   return (
   <>
-  <Navbar/>
-  <Routes>
-    <Route path='/' element={<Home/>} />
-    <Route path='/about' element={<About/>} />
-    <Route path='/menu' element={<Menu/>} />
-    <Route path='/pages' element={<Pages/>} />
-    <Route path='/contact' element={<Contact/>} />
-    <Route path='/booktable' element={<BookTable/>} />
-    <Route path='/menu/all' element={<Menu/>} />
-    <Route path='/menu/breakfast' element={<Menu/>} />
-    <Route path='/menu/maindishes' element={<Menu/>} />
-    <Route path='/menu/drinks' element={<Menu/>} />
-    <Route path='/menu/desserts' element={<Menu/>} />
-  </Routes>
-  <Footer/>
+  <RouterProvider router={router} />
   </>
   )
 }
---
+++ src/pages/Erorr404.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Erorr404 = () => {
+  return (
+    <div className='m-20 text-center space-y-4'>
+      <h1 className='text-7xl font-dmsans text-red-primary'>
+        404 Sorry Page Not Found
+      </h1>
+      <p className='text-gray-500 text-2xl'>
+        Please check Your internet connection & Refresh the page!
+      </p>
+    </div>
+  )
+}
+
+export default Erorr404
